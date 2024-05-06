8545c71e5a5c2a15680811f3484ab9f1444003de
Statist808 <163011257+Statist808@users.noreply.github.com>
2024-05-06 15:59:30 +0300

    Update модуль 5 задание 2.py

--- модуль 5 задание 2.py
+++ модуль 5 задание 2.py
@@ -1,8 +1,25 @@
-class save():
-    import json
-    try:
-        with open('1.json', "r") as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
+class House():
+    """описание дома"""
+    def __init__(self, street, number):
+      """свойства дома"""
+        self.street = street
+        self.number = number
+
+    def build(self):
+        """сторится дом"""
+        print("дом на улице" + self.street + "под номером" + str(self.number + "построен.")
+House1 = House("московская", 20)
+House2 = House("московская", 21)
+    def __dict__(House):
+        return {House}
+
+import json
+object = House()
+json.dumps(object.__dict__)
+    with open('1.json', "r") as f:
+        data = json.load(f)
+except FileNotFoundError:
+    data = {}
+
+
 
