09f4ab2003ce8aae697d73df396cdd639bfe273b
Statist808 <163011257+Statist808@users.noreply.github.com>
2024-05-13 21:12:35 +0300

    Update модуль 5 задание 2.py

--- модуль 5 задание 2.py
+++ модуль 5 задание 2.py
@@ -1,27 +1,10 @@
-class House():
-    '''описание дома'''
-    def __init__(self, street, number):
-        '''свойства дома'''
-        self.street = street
-        self.number = number
-
-    def build(self):
-        '''строиться дом'''
-        print('дом на улице' + self.street + 'под номером' + self.number + " построен.")
-House1 = House("московская", 20)
-House2 = House('московская', 21)
-
-print(House2.number)
 import json
-
-# Создание словаря
-data = {}
-# Преобразование словаря в JSON-строку
-json_string = json.dumps(data)
-
-# Запись JSON-строки в .json-файл
-with open("data.json", "w") as json_file:
-    json_file.write(json_string)
-
-
-
+class Model:
+         a = 1
+         b = 2
+         def save(self):
+             dct = {k: str(v) for k, v in vars(Model).items()}
+             with open('data.json', 'w', encoding='utf-8') as fout:
+                json.dump(dct, fout)
+obj = Model()
+obj.save()
