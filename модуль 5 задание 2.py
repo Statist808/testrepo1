fe1bf98eb3c32a120df5a55e0cf48e2d273797ce
Statist808 <163011257+Statist808@users.noreply.github.com>
2024-06-03 15:24:08 +0300

    Update модуль 5 задание 2.py

--- модуль 5 задание 2.py
+++ модуль 5 задание 2.py
@@ -3,7 +3,7 @@ class Model:
          a = 1
          b = 2
          def save(self):
-             dct = {k: str(v) for k, v in vars(Model).items()}
+             dct = {"a": 1, "b": 2}
              with open('data.json', 'w', encoding='utf-8') as fout:
                 json.dump(dct, fout)
 obj = Model()
