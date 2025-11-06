633e51e2fc6041cb686405373b06acc0ad50b900
Statist808 <163011257+Statist808@users.noreply.github.com>
2025-11-06 23:58:42 +0300

    Add files via upload

---
+++ готовое.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+
+url ="https://mfd.ru/currency/?currency=USD"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "lxml")
+table = soup.find("table", class_="mfd-table mfd-currency-table").text
+print(table)
