ec23864459fcd0d329237bfe5c80f363fcbc7226
Statist808 <163011257+Statist808@users.noreply.github.com>
2025-11-06 23:56:14 +0300

    Delete тренировка.py

--- тренировка.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import lxml
-
-url ="https://mfd.ru/currency/?currency=USD"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "lxml")
-table = soup.find("table", class_="mfd-table mfd-currency-table").text
-print(table)
