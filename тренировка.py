19aea46cf74ed8e1a9c69a6865fc8683d8135198
Statist808 <163011257+Statist808@users.noreply.github.com>
2025-11-06 23:21:58 +0300

    Update тренировка.py

--- тренировка.py
+++ тренировка.py
@@ -2,9 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import lxml
 
-url = "https://mfd.ru/currency/?currency=USD"
+url ="https://mfd.ru/currency/?currency=USD"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "lxml")
-table= soup.find("table", class_="mfd-table mfd-currency-table")
-data = table.find_all("td")
-print(data)
+table = soup.find("table", class_="mfd-table mfd-currency-table").text
+print(table)
